ba3ba599291c2e8e8caa1f581f4a8453adf6663d
Hojiakbarxon Olimxo'jayev <olimxojayev22.2007@gmail.com>
2026-08-18 21:57:16 +0500

    I changed mail service because of deployment conditions.

--- src/utils/mail.service.ts
+++ src/utils/mail.service.ts
@@ -1,24 +1,45 @@
-import * as nodemailer from "nodemailer"
-import { envConfig } from "./env.config"
-export async function sendMail(user: string, message: string) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        secure: true,
-        host: envConfig.mail.host,
-        port: envConfig.mail.port,
-        auth: {
-            user: envConfig.mail.user,
-            pass: envConfig.mail.password
-        }
+// import * as nodemailer from "nodemailer"
+// import { envConfig } from "./env.config"
+// export async function sendMail(user: string, message: string) {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         secure: true,
+//         host: envConfig.mail.host,
+//         port: envConfig.mail.port,
+//         auth: {
+//             user: envConfig.mail.user,
+//             pass: envConfig.mail.password
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: envConfig.mail.user,
+//         to: user,
+//         subject: "Movie",
+//         text: message
+//     };
+
+//     const res = await transporter.sendMail(mailOptions);
+//     return res
+// }
+
+
+// The website I used to deploy render.com blocks smtp to send emails, so I decided to change sending emails.
+export const sendMail = async (recipientEmail, otpCode) => {
+    const response = await fetch('https://api.resend.com/emails', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            from: 'onboarding@resend.dev',
+            to: recipientEmail,
+            subject: 'Your OTP Code for Movies web-site',
+            html: `<p>Your code is: <strong>${otpCode}</strong></p>`,
+        }),
     });
 
-    const mailOptions = {
-        from: envConfig.mail.user,
-        to: user,
-        subject: "Movie",
-        text: message
-    };
-
-    const res = await transporter.sendMail(mailOptions);
-    return res
-}
+    const data = await response.json();
+    console.log('Sent:', data);
+};
